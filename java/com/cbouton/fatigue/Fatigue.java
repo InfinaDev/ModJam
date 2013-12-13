f0abc030354e1489a285bf8fddf95379e3dfe748
C. Bouton <admin@cbouton.com>
2013-12-13 02:20:30 -0500

    Add notes for nh_99
    Signed-off-by: C. Bouton <admin@cbouton.com>

--- java/com/cbouton/fatigue/Fatigue.java
+++ java/com/cbouton/fatigue/Fatigue.java
@@ -26,3 +26,22 @@ public class Fatigue
         System.out.println(Statics.MODID + " Version " + Statics.VERSION + " Loaded");
     }
 }
+/*
+ * Automatic regen on peaceful Easy increases at double and decreases by half
+ * Normal increases and decreases by the default amounts Hard increases at half
+ * and decrease by double
+ * 
+ * 1/2 fatigue is normal full fatigue gives Haste/Speed no fatigue gives
+ * Slowness/Mining Fatigue
+ * 
+ * 10 steps/1 mining Operation is the same decrease
+ * 
+ * above 50% hunger decreases slower/below 50% hunger decreases faster Hard has
+ * players spawn at 50% fatigue
+ * 
+ * Sleeping is the best way to increase fatigue Mounted for a full day increases
+ * fatigue by 1/4 of sleeping if not moving. if moving, fatigue decreases at 50%
+ * the difficulty rate
+ * 
+ * Values can be determined later.
+ */
