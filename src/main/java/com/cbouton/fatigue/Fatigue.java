8e380f1c0a382760d46bbcee7a952ada090fa12d
nh_99 <ifence9@gmail.com>
2013-12-13 17:02:57 -0500

    Implementing coffee in the mod

--- src/main/java/com/cbouton/fatigue/Fatigue.java
+++ src/main/java/com/cbouton/fatigue/Fatigue.java
@@ -1,26 +1,29 @@
 package com.cbouton.fatigue;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.ForgeSubscribe;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
 import com.cbouton.fatigue.events.playerEvent;
+import com.cbouton.fatigue.items.CoffeeSeedsItem;
 import com.cbouton.fatigue.lib.Statics;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = Statics.MODID, version = Statics.VERSION)
-public class Fatigue
-{
+public class Fatigue {
 	public int difficulty;
+	public static final Item coffeeSeeds = new CoffeeSeedsItem(5479);
     
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
 		//TODO: Register Movement, Player Sleep and Item Use Events.
     	MinecraftForge.EVENT_BUS.register(new playerEvent());
+    	
     	//TODO: Get game mode updates
     	difficulty = Minecraft.getMinecraft().theWorld.difficultySetting;
         System.out.println(Statics.MODID + " Version " + Statics.VERSION + " Loaded");
