46217feeb771cf4720b3ab2be8b0dc6232809212
nh_99 <ifence9@gmail.com>
2013-12-14 17:51:46 -0500

    Hot coffee now gives haste for 30 seconds

--- src/main/java/com/cbouton/fatigue/items/HotCoffeeItem.java
+++ src/main/java/com/cbouton/fatigue/items/HotCoffeeItem.java
@@ -1,16 +1,29 @@
 package com.cbouton.fatigue.items;
 
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
-public class HotCoffeeItem extends Item {
+public class HotCoffeeItem extends ItemFood {
 
-	public HotCoffeeItem(int id) {
-		super(id);
+	public HotCoffeeItem(int id, int healthRegen, float probabilityOfPotionEffect, boolean canAlwaysEat) {
+		super(id, healthRegen, probabilityOfPotionEffect, canAlwaysEat);
 		setMaxStackSize(1);
 		setCreativeTab(CreativeTabs.tabFood);
 		setUnlocalizedName("hotCoffee");
 		setTextureName("fatigue:hotCoffee");
+	}
+	
+	@Override
+	public ItemStack onEaten(ItemStack stack, World world, EntityPlayer player) {
+		super.onEaten(stack, world, player);
+		
+		player.addPotionEffect(new PotionEffect(Potion.moveSpeed.getId(), 600, 3));
+		return stack;
 	}
 
 }
