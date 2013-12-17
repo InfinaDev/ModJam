fb037b05fc2c32dee37c10f1b28011f98707cdc6
C. Bouton <admin@cbouton.com>
2013-12-16 21:36:57 -0500

    Fix merge error.
    Signed-off-by: C. Bouton <admin@cbouton.com>

---
+++ src/main/java/com/cbouton/fatigue/handlers/FatigueTickHandler.java
@@ -0,0 +1,61 @@
+package com.cbouton.fatigue.handlers;
+
+import java.util.EnumSet;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
+
+import cpw.mods.fml.common.ITickHandler;
+import cpw.mods.fml.common.TickType;
+
+public class FatigueTickHandler implements ITickHandler {
+
+	@Override
+	public String getLabel() {
+		return "FatigueTickHandler";
+	}
+
+	@Override
+	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
+	}
+
+	@SuppressWarnings("unused")
+	@Override
+	public void tickStart(EnumSet<TickType> type, Object... tickData) {
+		EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+		
+		if (player != null) {
+			if (FatigueHandler.getFatigue(player.username) < 3000
+					&& !player.capabilities.isCreativeMode) {
+				player.addPotionEffect(new PotionEffect(Potion.digSlowdown
+						.getId(), 20, 2));
+				player.addPotionEffect(new PotionEffect(Potion.moveSlowdown
+						.getId(), 20, 2));
+				player.removePotionEffect(Potion.moveSpeed.getId());
+				player.removePotionEffect(Potion.digSpeed.getId());
+			} else if (FatigueHandler.getFatigue(player.username) > 54000
+					&& !player.capabilities.isCreativeMode) {
+				player.addPotionEffect(new PotionEffect(
+						Potion.digSpeed.getId(), 20, 1));
+				player.addPotionEffect(new PotionEffect(Potion.moveSpeed
+						.getId(), 20, 1));
+				player.removePotionEffect(Potion.moveSlowdown.getId());
+				player.removePotionEffect(Potion.digSlowdown.getId());
+			} else {
+				player.removePotionEffect(Potion.moveSpeed.getId());
+				player.removePotionEffect(Potion.digSpeed.getId());
+				player.removePotionEffect(Potion.moveSlowdown.getId());
+				player.removePotionEffect(Potion.digSlowdown.getId());
+			}
+
+		}
+	}
+
+	@Override
+	public EnumSet<TickType> ticks() {
+		return EnumSet.of(TickType.CLIENT);
+	}
+
+}
