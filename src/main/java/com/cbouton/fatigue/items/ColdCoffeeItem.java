7d5106a59442489b9de8a380262a725616fe1cda
nh_99 <ifence9@gmail.com>
2013-12-19 20:00:47 -0500

    Return cups to player(Fixes #6)

--- src/main/java/com/cbouton/fatigue/items/ColdCoffeeItem.java
+++ src/main/java/com/cbouton/fatigue/items/ColdCoffeeItem.java
@@ -1,15 +1,19 @@
 package com.cbouton.fatigue.items;
 
 import com.cbouton.fatigue.handlers.FatigueHandler;
+import com.cbouton.fatigue.lib.ItemStatics;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
 public class ColdCoffeeItem extends ItemFood {
+	Item woodenMug = new WoodenMugItem(ItemStatics.ITEM_WOODEN_MUG);
+	ItemStack woodMug = new ItemStack(woodenMug);
 
 	public ColdCoffeeItem(int id, int hungerRegen,
 			float probabilityOfPotionEffect, boolean isWolfsFavorite) {
@@ -27,7 +31,7 @@ public class ColdCoffeeItem extends ItemFood {
 		
 		FatigueHandler.increaseFatigue(player, 1000);
 		
-		return stack;
+		return woodMug;
 	}
 	
 	public EnumAction getItemUseAction(ItemStack par1ItemStack)
--- src/main/java/com/cbouton/fatigue/items/HotCoffeeItem.java
+++ src/main/java/com/cbouton/fatigue/items/HotCoffeeItem.java
@@ -1,10 +1,12 @@
 package com.cbouton.fatigue.items;
 
 import com.cbouton.fatigue.handlers.FatigueHandler;
+import com.cbouton.fatigue.lib.ItemStatics;
 
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemFood;
 import net.minecraft.item.ItemStack;
 import net.minecraft.potion.Potion;
@@ -12,6 +14,8 @@ import net.minecraft.potion.PotionEffect;
 import net.minecraft.world.World;
 
 public class HotCoffeeItem extends ItemFood {
+	Item woodenMug = new WoodenMugItem(ItemStatics.ITEM_WOODEN_MUG);
+	ItemStack woodMug = new ItemStack(woodenMug);
 
 	public HotCoffeeItem(int id, int hungerRegen,
 			float probabilityOfPotionEffect, boolean isWolfsFavorite) {
@@ -32,7 +36,7 @@ public class HotCoffeeItem extends ItemFood {
 		
 		player.addPotionEffect(new PotionEffect(Potion.moveSpeed.getId(), 600,
 				3));
-		return stack;
+		return woodMug;
 	}
 
 	public EnumAction getItemUseAction(ItemStack par1ItemStack) {
