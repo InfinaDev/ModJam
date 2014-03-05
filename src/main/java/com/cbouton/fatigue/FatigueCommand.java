897272f4154e60e93ded6a875171460ff8aa9c90
C. Bouton <admin@cbouton.com>
2014-03-05 18:06:18 -0500

    First Updates to 1.7
    Resolved errors in FatigueCommand and FatigueTickHandler

--- src/main/java/com/cbouton/fatigue/FatigueCommand.java
+++ src/main/java/com/cbouton/fatigue/FatigueCommand.java
@@ -1,25 +1,25 @@
 package com.cbouton.fatigue;
 
-import java.util.List;
-
 import com.cbouton.fatigue.handlers.FatigueHandler;
-
 import net.minecraft.client.Minecraft;
+import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.command.WrongUsageException;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.ChatMessageComponent;
+import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.EnumChatFormatting;
 
-public class FatigueCommand implements ICommand {
+import java.util.List;
+
+public class FatigueCommand extends CommandBase {
 
 	public FatigueCommand() {
 	}
 
 	@Override
 	public int compareTo(Object o) {
-		// TODO Auto-generated method stub
-		return 0;
+        return this.getCommandName().compareTo(((ICommand) o).getCommandName());
 	}
 
 	@Override
@@ -39,9 +39,9 @@ public class FatigueCommand implements ICommand {
 	}
 
 	@Override
-	public void processCommand(ICommandSender icommandsender, String[] astring) {
-		if (icommandsender instanceof EntityPlayer) {
-			EntityPlayer player = (EntityPlayer) icommandsender;
+	public void processCommand(ICommandSender sender, String[] astring) {
+		if (sender instanceof EntityPlayer) {
+			EntityPlayer player = (EntityPlayer) sender;
 			if (astring.length == 2) {
 				int amount = Integer.parseInt(astring[1]);
 				if (astring[0].equalsIgnoreCase("add")) {
@@ -49,10 +49,10 @@ public class FatigueCommand implements ICommand {
 				} else if (astring[0].equalsIgnoreCase("subtract")) {
 					FatigueHandler.decreaseFatigue(player, amount);
 				} else {
-					player.sendChatToPlayer(ChatMessageComponent.createFromText(getCommandUsage(icommandsender)).setColor(EnumChatFormatting.RED));
+					throw new WrongUsageException(getCommandUsage(sender));
 				}
 			} else {
-				player.sendChatToPlayer(ChatMessageComponent.createFromText(getCommandUsage(icommandsender)).setColor(EnumChatFormatting.RED));
+                throw new WrongUsageException(getCommandUsage(sender));
 			}
 		}
 
--- src/main/java/com/cbouton/fatigue/handlers/FatigueTickHandler.java
+++ src/main/java/com/cbouton/fatigue/handlers/FatigueTickHandler.java
@@ -1,61 +1,48 @@
 package com.cbouton.fatigue.handlers;
 
-import java.util.EnumSet;
-
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.TickEvent;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 
-import cpw.mods.fml.common.ITickHandler;
-import cpw.mods.fml.common.TickType;
+import java.util.EnumSet;
 
-public class FatigueTickHandler implements ITickHandler {
+public class FatigueTickHandler{
 
-	@Override
-	public String getLabel() {
-		return "FatigueTickHandler";
-	}
+	@SubscribeEvent
+	public void tickStart(TickEvent.ServerTickEvent event) {
+		String[] players = Minecraft.getMinecraft().getIntegratedServer().getAllUsernames();
 
-	@Override
-	public void tickEnd(EnumSet<TickType> type, Object... tickData) {
-	}
+        for (int i=0; i<players.length; i++){
+		    EntityPlayer player = Minecraft.getMinecraft().getIntegratedServer().getConfigurationManager().getPlayerForUsername(players[i]);
+		    if (player != null) {
+			    if (FatigueHandler.getFatigue(players[i]) < 3000
+					    && !player.capabilities.isCreativeMode) {
+				    player.addPotionEffect(new PotionEffect(Potion.digSlowdown
+						    .getId(), 20, 2));
+				    player.addPotionEffect(new PotionEffect(Potion.moveSlowdown
+						.   getId(), 20, 2));
+				    player.removePotionEffect(Potion.moveSpeed.getId());
+				    player.removePotionEffect(Potion.digSpeed.getId());
+			    } else if (FatigueHandler.getFatigue(players[i]) > 54000
+					    && !player.capabilities.isCreativeMode) {
+				    player.addPotionEffect(new PotionEffect(
+						    Potion.digSpeed.getId(), 20, 1));
+				    player.addPotionEffect(new PotionEffect(Potion.moveSpeed
+						    .getId(), 20, 1));
+				    player.removePotionEffect(Potion.moveSlowdown.getId());
+				    player.removePotionEffect(Potion.digSlowdown.getId());
+			    } else {
+				    player.removePotionEffect(Potion.moveSpeed.getId());
+				    player.removePotionEffect(Potion.digSpeed.getId());
+				    player.removePotionEffect(Potion.moveSlowdown.getId());
+				    player.removePotionEffect(Potion.digSlowdown.getId());
+			    }
 
-	@SuppressWarnings("unused")
-	@Override
-	public void tickStart(EnumSet<TickType> type, Object... tickData) {
-		EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-		
-		if (player != null) {
-			if (FatigueHandler.getFatigue(player.username) < 3000
-					&& !player.capabilities.isCreativeMode) {
-				player.addPotionEffect(new PotionEffect(Potion.digSlowdown
-						.getId(), 20, 2));
-				player.addPotionEffect(new PotionEffect(Potion.moveSlowdown
-						.getId(), 20, 2));
-				player.removePotionEffect(Potion.moveSpeed.getId());
-				player.removePotionEffect(Potion.digSpeed.getId());
-			} else if (FatigueHandler.getFatigue(player.username) > 54000
-					&& !player.capabilities.isCreativeMode) {
-				player.addPotionEffect(new PotionEffect(
-						Potion.digSpeed.getId(), 20, 1));
-				player.addPotionEffect(new PotionEffect(Potion.moveSpeed
-						.getId(), 20, 1));
-				player.removePotionEffect(Potion.moveSlowdown.getId());
-				player.removePotionEffect(Potion.digSlowdown.getId());
-			} else {
-				player.removePotionEffect(Potion.moveSpeed.getId());
-				player.removePotionEffect(Potion.digSpeed.getId());
-				player.removePotionEffect(Potion.moveSlowdown.getId());
-				player.removePotionEffect(Potion.digSlowdown.getId());
-			}
-
-		}
-	}
-
-	@Override
-	public EnumSet<TickType> ticks() {
-		return EnumSet.of(TickType.CLIENT);
+		    }
+        }
 	}
 
 }
